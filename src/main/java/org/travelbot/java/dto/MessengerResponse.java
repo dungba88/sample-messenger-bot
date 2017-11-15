ce7dc8dd448b611c77ab98511d2c5eab17904851
Dzung Bui <dungba.sg@gmail.com>
2017-11-16 00:59:06 +0800

    add dummy property

--- src/main/java/org/travelbot/java/dto/MessengerResponse.java
+++ src/main/java/org/travelbot/java/dto/MessengerResponse.java
@@ -4,4 +4,10 @@ import org.joo.scorpius.support.BaseResponse;
 public class MessengerResponse extends BaseResponse {
 
 	private static final long serialVersionUID = 8485044599299739354L;
+	
+	private String text;
+	
+	public String getText() {
+		return text;
+	}
 }
