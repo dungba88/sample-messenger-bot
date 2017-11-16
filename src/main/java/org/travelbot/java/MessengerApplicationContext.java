6ffddb37f6a9468005770dd565b43e61cbce95e3
Dzung Bui <dungba.sg@gmail.com>
2017-11-16 22:17:32 +0800

    change to default http client

--- src/main/java/org/travelbot/java/MessengerApplicationContext.java
+++ src/main/java/org/travelbot/java/MessengerApplicationContext.java
@@ -1,15 +1,7 @@
 package org.travelbot.java;
 
-import java.io.IOException;
 import java.util.Optional;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.joo.scorpius.ApplicationContext;
 import org.joo.scorpius.support.BaseResponse;
 import org.joo.scorpius.support.TriggerExecutionException;
@@ -20,7 +12,6 @@ import org.joo.scorpius.support.deferred.AsyncDeferredObject;
 import org.joo.scorpius.support.deferred.Deferred;
 
 import com.github.messenger4j.Messenger;
-import com.github.messenger4j.spi.MessengerHttpClient;
 
 public class MessengerApplicationContext extends ApplicationContext {
 
@@ -37,21 +28,21 @@ public class MessengerApplicationContext extends ApplicationContext {
 			Factory<Optional<String>> idGenerator) {
 		super(deferredFactory, executionContextBuilderFactory, idGenerator);
 		
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-		
-		MessengerHttpClient customClient = new MessengerHttpClient() {
-			
-			@Override
-			public HttpResponse execute(HttpMethod httpMethod, String url, String jsonBody) throws IOException {
-				HttpPost request = new HttpPost(url);
-				HttpEntity entity = new ByteArrayEntity(jsonBody.getBytes());
-		        request.setEntity(entity);
-		        CloseableHttpResponse response = httpClient.execute(request);
-		        String body = EntityUtils.toString(response.getEntity());
-				return new HttpResponse(response.getStatusLine().getStatusCode(), body);
-			}
-		};
-		messenger = Messenger.create(ACCESS_TOKEN, APP_SECRET, VERIFY_TOKEN, Optional.of(customClient));
+//		final CloseableHttpClient httpClient = HttpClients.createDefault();
+//		
+//		final MessengerHttpClient customClient = new MessengerHttpClient() {
+//			
+//			@Override
+//			public HttpResponse execute(HttpMethod httpMethod, String url, String jsonBody) throws IOException {
+//				HttpPost request = new HttpPost(url);
+//				HttpEntity entity = new ByteArrayEntity(jsonBody.getBytes());
+//		        request.setEntity(entity);
+//		        CloseableHttpResponse response = httpClient.execute(request);
+//		        String body = EntityUtils.toString(response.getEntity());
+//				return new HttpResponse(response.getStatusLine().getStatusCode(), body);
+//			}
+//		};
+		messenger = Messenger.create(ACCESS_TOKEN, APP_SECRET, VERIFY_TOKEN);
 	}
 	
 	public MessengerApplicationContext() {
