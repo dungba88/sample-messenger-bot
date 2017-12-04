df8e72dd05ee02d15a9f50c52843e4910ce11ca5
Dzung Bui <dungba.sg@gmail.com>
2017-12-04 16:19:48 +0800

    refactor

--- src/main/java/org/travelbot/java/MessengerCompositionBootstrap.java
+++ src/main/java/org/travelbot/java/MessengerCompositionBootstrap.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.apache.logging.log4j.core.config.plugins.util.PluginManager;
 import org.joo.scorpius.Bootstrap;
-import org.joo.scorpius.support.bootstrap.AbstractBootstrap;
 import org.joo.scorpius.support.bootstrap.CompositionBootstrap;
 import org.joo.scorpius.support.builders.contracts.IdGenerator;
 import org.joo.scorpius.support.builders.id.TimeBasedIdGenerator;
@@ -36,7 +35,8 @@ public class MessengerCompositionBootstrap extends CompositionBootstrap {
     protected void configureBootstraps(List<Bootstrap> bootstraps) {
         MessengerApplicationContext msgApplicationContext = (MessengerApplicationContext) applicationContext;
 
-        bootstraps.add(AbstractBootstrap.from(() -> configureOverridens(msgApplicationContext)));
+        configureOverridens(msgApplicationContext);
+
         bootstraps.add(new TypeSafeBootstrap());
         bootstraps.add(new TriggerTypeSafeBootstrap());
         bootstraps.add(new TriggerConfigurator());
